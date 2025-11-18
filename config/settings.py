d0ab79e11aa5c274f7b0f69bf82d9ec750c751e6
NathaHernandez <shernandez32@udi.edu.co>
2025-11-17 21:12:17 -0500

    habilitar host

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-0712o#64iqh^f%r^i_tq$31phj!fn3$kt^$k-fq(n3tj7$ujll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
