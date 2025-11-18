c10123d2c5327defac0de12a6d0556100d7e777b
NathaHernandez <shernandez32@udi.edu.co>
2025-11-17 22:33:07 -0500

    arreglar url registro

--- gateway/urls.py
+++ gateway/urls.py
@@ -3,11 +3,12 @@ from .views import (
     AuthLoginProxyView,
     AuthSessionStatusProxyView,
     AuthLogoutProxyView,
+    AuthRegisterProxyView
 )
 
 urlpatterns = [
     path('auth/login/', AuthLoginProxyView.as_view()),
-    path('auth/register/', AuthLoginProxyView.as_view()),
+    path('auth/register/', AuthRegisterProxyView.as_view()),
     path('auth/session-status/', AuthSessionStatusProxyView.as_view()),
     path('auth/logout/', AuthLogoutProxyView.as_view()),
 ]
